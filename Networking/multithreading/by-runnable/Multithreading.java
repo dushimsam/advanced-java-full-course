392a4e188ca0866abb9e626eb738fb626219987f
dushimsam <dushsam100@gmail.com>
2022-06-20 18:54:13 +0200

    multithreading using Runnable , fund. methods

--- Networking/multithreading/by-runnable/Multithreading.java
+++ Networking/multithreading/by-runnable/Multithreading.java
@@ -2,10 +2,45 @@ public class Multithreading {
 
     public static void main(String[] args) throws Exception {
 
-        for (int i = 1; i < 3; i++) {
+        
             MultithreadingThing thing = new MultithreadingThing();
-            Thread thread = new Thread(thing);
-            thread.start();
+            Thread thread1 = new Thread(thing);
+            thread1.setName("Thread - 1");
+            
+            Thread thread2 = new Thread(thing);
+            thread2.setName("Thread - 2");
+
+            
+            // Get the priority of the thread
+            // By default the both threads have the same priority
+            // Range of priorities  1 - 10
+            // 1 - means lowest priority
+            // 10 - means highest priority
+            // 5 - general priority
+            thread2.setPriority(10);
+
+
+            // Get the name of the thread
+            System.out.println("Thread's name is " + thread1.getName());
+
+            thread1.start();
+            thread2.start();
+          
+            // Join avoids the multithreading behaviour , waits for one thread to finish
+            // before it takes another one
+            thread1.join();
+
+
+            // Check if the thread is still running . In order words it has not completed
+            // it's intended iterations
+            System.out.println("Thread-1 is running  " + thread1.isAlive());
+
+            // Check the thread1's priority
+            System.out.println("Thread-1's priority is  " + thread1.getPriority());
+
+            // Number of the running threads
+            System.out.println("Number of threads "+Thread.activeCount());
+
         }
     }
-}
+
